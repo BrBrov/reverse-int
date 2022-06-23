9df8486497c1ebc4f2b5cd55f8f4ae062f28f272
Babeer <epoc@list.ru>
2022-06-23 13:26:56 +0300

    feat: Create revert number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  n =  Array.from(Math.abs(n).toString())
+  n = n.reverse().join('');  
+  return Number.parseInt(n, 10);
 }
+
+// let n = -192;
+// let {log} = console;
+// let b = Array.from(Math.abs(n).toString());
+// n = b.reverse().join('');
+// // n = +n;
+// log(typeof n);
+
